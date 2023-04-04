e60f7a20d8ac98d9b6f6557e20023f8d01f944ff
GJZ26 <adolfogj26@gmail.com>
2023-04-03 22:30:09 -0600

    Bullet fading

--- src/script/Entities/Bullet.js
+++ src/script/Entities/Bullet.js
@@ -1,6 +1,7 @@
 export class Bullet {
     localContext; id; x; y; angle; speed = 9;
     initial_x; initial_y;
+    distance; max_distance
     width = 10; alived;
 
     /**
@@ -8,7 +9,7 @@ export class Bullet {
      * @param {CanvasRenderingContext2D} context 
      * @param {*} id 
      */
-    constructor(context, id, position, angle) {
+    constructor(context, id, position, angle, max_distance = 1000) {
         this.localContext = context
         this.x = position.x
         this.y = position.y
@@ -19,23 +20,28 @@ export class Bullet {
         this.angle = angle
         this.id = id
         this.alived = true
+
+        this.max_distance = max_distance
     }
 
     draw() {
+        this.distance = Math.sqrt(Math.pow((this.initial_x - this.x), 2) + Math.pow((this.initial_y - this.y), 2))
+        let opacity = this.distance > this.max_distance ? 0 : 1 - (this.distance / this.max_distance)
 
-        if (this.initial_x - this.x > 1000 ||
-            this.initial_y - this.y > 1000 ||
-            this.initial_x - this.x < -1000 ||
-            this.initial_y - this.y < -1000) {
+        console.log(opacity)
+
+        if (this.distance > this.max_distance) {
             this.alived = false;
         }
 
         this.y = this.y + this.speed * Math.cos(this.angle - 1.5708 * 2)
         this.x = this.x + this.speed * Math.sin(this.angle)
         this.localContext.beginPath()
+        this.localContext.globalAlpha = opacity
         this.localContext.fillStyle = "#8D64D6";
         this.localContext.arc(this.x, this.y, this.width, 0, 2 * Math.PI)
         this.localContext.fill()
         this.localContext.closePath()
+        this.localContext.globalAlpha = 1
     }
 }
